dfb75f83ea203857c536ac8ae162d43ba12c8aa8
Byran Huang <byran8855@gmail.com>
2024-07-11 00:58:14 -0400

    added vercel configs

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,23 +1,23 @@
-import { defineConfig } from "astro/config"
-import mdx from "@astrojs/mdx"
-import sitemap from "@astrojs/sitemap"
-import tailwind from "@astrojs/tailwind"
-import solidJs from "@astrojs/solid-js"
-import markdownIntegration from '@astropub/md'
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
+import markdownIntegration from '@astropub/md';
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-sphere-demo.vercel.app",
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
-  integrations: [mdx(), sitemap(), solidJs(), markdownIntegration(), tailwind({ applyBaseStyles: false })],
+  site: "https://byran.ee",
+  integrations: [mdx(), sitemap(), solidJs(), markdownIntegration(), tailwind({
+    applyBaseStyles: false
+  })],
   markdown: {
     remarkPlugins: [],
-    rehypePlugins: [],
+    rehypePlugins: []
     // syntaxHighlight: 'shiki'
     // syntaxHighlight: 'prism'
-  }
-})
+  },
+  output: "static",
+  adapter: vercel()
+});
